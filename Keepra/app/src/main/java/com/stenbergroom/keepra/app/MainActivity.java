72d991521b605df05e9f061a39e4ef060203869b
Aleksandr Marchenko <marrchenko@gmail.com>
2015-04-22 20:05:36 +0300

    Create MainActivity class and layout file

---
+++ Keepra/app/src/main/java/com/stenbergroom/keepra/app/MainActivity.java
@@ -0,0 +1,16 @@
+package com.stenbergroom.keepra.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Sten on 22.04.2015.
+ */
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
