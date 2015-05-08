c4bb4a1fd520035de10a1fa9c77b8d032fe98028
Aleksandr Marchenko <marrchenko@gmail.com>
2015-05-08 17:49:48 +0300

    app structure

---
+++ Keepra/app/src/main/java/com/stenbergroom/keepra/app/DetailActivity.java
@@ -0,0 +1,9 @@
+package com.stenbergroom.keepra.app;
+
+import android.support.v7.app.ActionBarActivity;
+
+/**
+ * Created by Sten on 08.05.2015.
+ */
+public class DetailActivity extends ActionBarActivity {
+}
