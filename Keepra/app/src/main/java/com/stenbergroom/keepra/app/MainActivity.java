b8b3da47741d6a740c11355a2d81fbe9dd5047b0
Aleksandr Marchenko <marrchenko@gmail.com>
2015-05-08 18:35:52 +0300

    app structure

--- Keepra/app/src/main/java/com/stenbergroom/keepra/app/MainActivity.java
+++ Keepra/app/src/main/java/com/stenbergroom/keepra/app/MainActivity.java
@@ -1,13 +1,210 @@
 package com.stenbergroom.keepra.app;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.ResolveInfo;
+import android.graphics.Color;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v4.app.ActivityOptionsCompat;
+import android.support.v4.util.Pair;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.widget.*;
+import com.mikepenz.aboutlibraries.Libs;
+import com.mikepenz.iconics.IconicsDrawable;
+import com.mikepenz.iconics.typeface.FontAwesome;
+import com.stenbergroom.keepra.app.adapter.ApplicationAdapter;
+import com.stenbergroom.keepra.app.entity.AppInfo;
+import com.stenbergroom.keepra.app.itemanimator.CustomItemAnimator;
+import com.stenbergroom.keepra.app.util.UploadHelper;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class MainActivity extends ActionBarActivity {
+
+    private static final String LOG_TAG = "LOG_TAG";
+
+    private List<AppInfo> applicationList = new ArrayList<AppInfo>();
+
+    private ApplicationAdapter mAdapter;
+    private ImageButton mFabButton;
+    private RecyclerView mRecyclerView;
+    private SwipeRefreshLayout mSwipeRefreshLayout;
+    private ProgressBar mProgressBar;
+
+    private static UploadHelper.UploadComponentInfoTask uploadComponentInfoTask = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.d(LOG_TAG, " - MainActivity.onCreate");
+
+        // Handle Toolbar
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        // Handle DrawerLayout
+        DrawerLayout mDrawerLayout = (DrawerLayout)findViewById(R.id.drawer);
+
+        // Handle ActionBarDrawerToggle
+        ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, toolbar, R.string.drawer_open, R.string.drawer_close);
+        actionBarDrawerToggle.syncState();
+
+        // Handle different Drawer States :D
+        mDrawerLayout.setDrawerListener(actionBarDrawerToggle);
+
+        // Handle DrawerList
+        LinearLayout mDrawerList = (LinearLayout)findViewById(R.id.drawerList);
+
+        // Handle ProgressBar
+        mProgressBar = (ProgressBar)findViewById(R.id.progressBar);
+
+        // Init DrawerElems NOTE Just don't do this in live app :D
+        final SharedPreferences pref = getSharedPreferences("com.mikepenz.applicationreader", 0);
+        ((Switch)mDrawerList.findViewById(R.id.drawer_autoupload)).setChecked(pref.getBoolean("autouploadenabled", false));
+        ((Switch)mDrawerList.findViewById(R.id.drawer_autoupload)).setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                SharedPreferences.Editor editor = pref.edit();
+                editor.putBoolean("autouploadenabled", isChecked);
+                editor.apply();
+            }
+        });
+
+        mDrawerList.findViewById(R.id.drawer_opensource).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new Libs.Builder()
+                        .withFields(R.string.class.getFields())
+                        .withVersionShown(true)
+                        .withLicenseShown(true)
+                        .withActivityTitle(getString(R.string.drawer_opensource))
+                        .withActivityTheme(R.style.AboutTheme)
+                        .start(MainActivity.this);
+            }
+        });
+
+        ((ImageView)mDrawerList.findViewById(R.id.drawer_opensource_icon)).setImageDrawable(new IconicsDrawable(this, FontAwesome.Icon.faw_github).colorRes(R.color.secondary).actionBarSize());
+
+        // Fab Button
+        mFabButton = (ImageButton)findViewById(R.id.fab_button);
+        mFabButton.setImageDrawable(new IconicsDrawable(this, FontAwesome.Icon.faw_upload).color(Color.WHITE).actionBarSize());
+        mFabButton.setOnClickListener(fabClickListener);
+        com.stenbergroom.keepra.app.Utils.configureFab(mFabButton);
+
+        mRecyclerView = (RecyclerView)findViewById(R.id.list);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        mRecyclerView.setItemAnimator(new CustomItemAnimator());
+        //mRecyclerView.setItemAnimator(new ReboundItemAnimator());
+
+        mAdapter = new ApplicationAdapter(new ArrayList<AppInfo>(), R.layout.row_application, MainActivity.this);
+        mRecyclerView.setAdapter(mAdapter);
+
+        mSwipeRefreshLayout = (SwipeRefreshLayout)findViewById(R.id.swipe_container);
+        mSwipeRefreshLayout.setColorSchemeColors(getResources().getColor(R.color.theme_accent));
+        mSwipeRefreshLayout.setRefreshing(true);
+        mSwipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                Log.d(LOG_TAG, " - MainActivity.onCreate.onRefreshListener.onRefresh");
+                new InitializeApplicationTask().execute();
+            }
+        });
+
+        new InitializeApplicationTask().execute();
+
+        if(savedInstanceState != null){
+            if(uploadComponentInfoTask != null){
+                if(uploadComponentInfoTask.isRunning){
+                    uploadComponentInfoTask.showProgress(this);
+                }
+            }
+        }
+
+        // Show Progress
+        mRecyclerView.setVisibility(View.GONE);
+        mProgressBar.setVisibility(View.VISIBLE);
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        Log.d(LOG_TAG, " - MainActivity.onSaveInstanceState");
+        super.onSaveInstanceState(outState);
+    }
+
+    View.OnClickListener fabClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            uploadComponentInfoTask = UploadHelper.getInstance(MainActivity.this, applicationList).uploadAll();
+        }
+    };
+
+    public void animateActivity(AppInfo appInfo, View appIcon){
+        Log.d(LOG_TAG, " - MainActivity.animateActivity");
+        Intent i = new Intent(this, DetailActivity.class);
+        i.putExtra("appInfo", appInfo.getComponentName());
+
+        ActivityOptionsCompat transitionActivityOptions = ActivityOptionsCompat.makeSceneTransitionAnimation(this, Pair.create((View) mFabButton, "fab"), Pair.create(appIcon, "appIcon"));
+        startActivity(i, transitionActivityOptions.toBundle());
+    }
+
+    private class InitializeApplicationTask extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        protected void onPreExecute() {
+            Log.d(LOG_TAG, " - MainActivity.InitializeApplicationTask.onPreExecute");
+            mAdapter.clearApplications();
+            super.onPreExecute();
+        }
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            Log.d(LOG_TAG, " - MainActivity.InitializeApplicationTask.doInBackground");
+            applicationList.clear();
+
+            //Query the applications
+            final Intent mainIntent = new Intent(Intent.ACTION_MAIN, null);
+            mainIntent.addCategory(Intent.CATEGORY_LAUNCHER);
+
+            List<ResolveInfo> ril = getPackageManager().queryIntentActivities(mainIntent, 0);
+            for(ResolveInfo ri : ril){
+                applicationList.add(new AppInfo(MainActivity.this, ri));
+            }
+
+            Collections.sort(applicationList);
+
+            for(AppInfo appInfo : applicationList){
+                // Load icons before shown. so the list is smoother
+                appInfo.getIcon();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            Log.d(LOG_TAG, " - MainActivity.InitializeApplicationTask.onPostExecute");
+            // Handle visibility
+            mRecyclerView.setVisibility(View.VISIBLE);
+            mProgressBar.setVisibility(View.GONE);
+
+            // Set data for list
+            mAdapter.addApplications(applicationList);
+            mSwipeRefreshLayout.setRefreshing(false);
+
+            super.onPostExecute(aVoid);
+        }
     }
 }
