969150532c0caaf96f72b40c4f6ad4cfb37127c8
Aleksandr Marchenko <marrchenko@gmail.com>
2015-06-01 19:45:05 +0300

    app structure

--- Keepra/app/src/main/java/com/stenbergroom/keepra/app/Test_NewTaskActivity.java
+++ Keepra/app/src/main/java/com/stenbergroom/keepra/app/Test_NewTaskActivity.java
@@ -1,9 +1,12 @@
 package com.stenbergroom.keepra.app;
 
+import android.animation.Animator;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.view.ViewPropertyAnimator;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -59,11 +62,45 @@ public class Test_NewTaskActivity extends ActionBarActivity{
         });
     }
 
-    public void fillRow(View view, final String title, String hintText){
+    public void fillRow(View view, final String title, final String hintText){
         TextView titleView = (TextView)view.findViewById(R.id.title);
         titleView.setText(title);
 
         EditText descriptionView = (EditText)view.findViewById(R.id.description);
         descriptionView.setHint(hintText);
+    }
+
+    @Override
+    public void onBackPressed() {
+        for(int i = rowContainer.getChildCount() - 1; i >= 0; i--){
+            final View rowView = rowContainer.getChildAt(i);
+            ViewPropertyAnimator propertyAnimator = rowView.animate().setStartDelay((rowContainer.getChildCount() - 1 - i) * SCALE_DELAY).scaleX(0).scaleY(0);
+
+            propertyAnimator.setListener(new Animator.AnimatorListener() {
+                @Override
+                public void onAnimationStart(Animator animation) {
+
+                }
+
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP){
+                        finishAfterTransition();
+                    }else{
+                        finish();
+                    }
+                }
+
+                @Override
+                public void onAnimationCancel(Animator animation) {
+
+                }
+
+                @Override
+                public void onAnimationRepeat(Animator animation) {
+
+                }
+            });
+        }
     }
 }
